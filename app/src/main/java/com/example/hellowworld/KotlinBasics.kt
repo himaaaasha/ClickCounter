c13547c1111b6cf5a68d6cf9690e8dcf0e792df5
hikalk <himasha.abeywickrama95@gmail.com>
2021-06-25 01:28:58 +0530

    Kotlin Number Types

---
+++ app/src/main/java/com/example/hellowworld/KotlinBasics.kt
@@ -0,0 +1,16 @@
+package com.example.hellowworld
+
+fun main(){
+    //Integer types Byte(8 bit), Short(16 bit), Int(32 bit), Long(64 bit)
+    val myByte: Byte = 23
+    val myShort: Short = 212
+    val myInt: Int = 234234234
+    val myLong: Long = 234234242342342342
+
+    //Floating point number types: Float(32 bit), Double (64 bit)
+    val myFloat: Float = 13.23F
+    val myDouble: Double = 234.42342
+
+    var myName = "Himasha"
+    print("Hello $myName")
+}
